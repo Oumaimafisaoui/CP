59bdacf30f41f01b1410f792b13f7ec5025abade
Oumaima Fisaoui <oufisaou@e1r13p4.1337.ma>
2023-02-07 11:18:57 +0100

    finished week 1

--- week1/Two_intervals.cpp
+++ week1/Two_intervals.cpp
@@ -11,21 +11,13 @@ int main()
     cin >> l1 >> r1 >> l2 >> r2;
  
    if (l2 >= l1 && r1 >= l2 && r1 <= r2)
-   {
         cout << l2 << " " << r1 << endl;
-   }
    else if (l2 >= l1 && r2 <= r1 && r2 >= l2)
-   {
     cout << l2 << " " << r2 << endl;
-   }
    else if (l1 >= l2 && r2 <= r1 && l1 <= r2)
-   {
         cout << l1 << " " << r2 << endl;
-   }
    else if (l1 >= l2 && r1 <= r2 && r1 > l2)
-    {
         cout << l1 << " " << r1 << endl;
-    }
     else
         cout << -1 << endl;
     return (0);
